44bdd9f3ed9e00757a4d7014a5c89be3dea449b5
Tia Rose <shortyshort789@aol.com>
2019-10-21 17:27:45 -0700

    added comment

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 import Button from "../components/button/button";
